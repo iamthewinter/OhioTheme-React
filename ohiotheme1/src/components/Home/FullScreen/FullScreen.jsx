28013ee492dcdb64b7d533d5e111b76db8c6b313
Azer Mammadov <mammadovazer98@gmail.com>
2023-02-12 19:00:31 +0400

    Some responsive changes in first section

--- ohiotheme1/src/components/Home/FullScreen/FullScreen.jsx
+++ ohiotheme1/src/components/Home/FullScreen/FullScreen.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import FullScrnStyle from "../FullScreen/FullScreen.module.scss";
 const FullScreen = () => {
-  return <div>FullScreen</div>;
+  return (
+    <section className={FullScrnStyle.third_section}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xl-4">
+            <div className={FullScrnStyle.fluid_banner}></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FullScreen;
