f6b7c79c04ec476f110d62c522b6ae46af5c6c7a
Azer Mammadov <mammadovazer98@gmail.com>
2023-02-15 18:42:44 +0400

    Some changes

---
+++ ohiotheme1/src/components/Contact/AllContacts/AllContacts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AllContStyle from "../AllContacts/AllContact.module.scss";
+const AllContacts = () => {
+  return (
+    <section className={AllContStyle.allcontacts_section}>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AllContacts;
--- ohiotheme1/src/components/Contact/Contact.jsx
+++ ohiotheme1/src/components/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AllContacts from "./AllContacts/AllContacts";
 import SendEmail from "./SendEmail/SendEmail";
 
 const Contact = () => {
   return (
     <>
       <SendEmail />
+      <AllContacts />
     </>
   );
 };
