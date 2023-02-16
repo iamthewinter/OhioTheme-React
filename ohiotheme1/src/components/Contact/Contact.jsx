27701683a8b0c796bcbe08493d4191a77bfe1586
Azer Mammadov <mammadovazer98@gmail.com>
2023-02-16 22:32:45 +0400

    Google Map added

--- ohiotheme1/src/components/Contact/Contact.jsx
+++ ohiotheme1/src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AllContacts from "./AllContacts/AllContacts";
+import IframeMap from "./IframeMap/IframeMap";
 import SendEmail from "./SendEmail/SendEmail";
 
 const Contact = () => {
@@ -7,6 +8,7 @@ const Contact = () => {
     <>
       <SendEmail />
       <AllContacts />
+      <IframeMap />
     </>
   );
 };
---
+++ ohiotheme1/src/components/Contact/IframeMap/IframeMap.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import IframeMapStyle from "../IframeMap/Map.module.scss";
+
+const IframeMap = () => {
+  return (
+    <section className={IframeMapStyle.map_iframe}>
+      <div className="mapouter">
+        <div className="gmap_canvas">
+          <iframe
+            width="100%"
+            height="397px"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=columbus&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            frameBorder="0"
+            scrolling="no"
+          ></iframe>
+          <br />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default IframeMap;
