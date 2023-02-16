e4c5bbd17911b493b7f19dce7736ff3ed8908b55
Azer Mammadov <mammadovazer98@gmail.com>
2023-02-16 22:51:01 +0400

    Careers section is done

--- ohiotheme1/src/components/Contact/IframeMap/IframeMap.jsx
+++ ohiotheme1/src/components/Contact/IframeMap/IframeMap.jsx
@@ -5,10 +5,9 @@ const IframeMap = () => {
   return (
     <section className={IframeMapStyle.map_iframe}>
       <div className="mapouter">
-        <div className="gmap_canvas">
+        <div className={IframeMapStyle.gmap_canvas}>
           <iframe
             width="100%"
-            height="397px"
             id="gmap_canvas"
             src="https://maps.google.com/maps?q=columbus&t=&z=13&ie=UTF8&iwloc=&output=embed"
             frameBorder="0"
